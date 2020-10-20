8a991f36efac07a4a3b13ce87abe072acc970b63
Eduard <leskov778@gmail.com>
2020-10-20 10:51:16 +0300

    add compression

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
+const compression = require('compression');
 const path = require('path');
 const app = express();
 const {v4} = require('uuid')
 
 let CONTACTS = [];
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded());
 app.set('port', process.env.PORT || 8080);
